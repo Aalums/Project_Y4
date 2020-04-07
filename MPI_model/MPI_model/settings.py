1da54949345602acb1a852a8c28b9b8c0a362299
phpond <59070182@it.kmitl.ac.th>
2020-04-07 14:41:36 +0700

    connectDB

--- MPI_model/MPI_model/settings.py
+++ MPI_model/MPI_model/settings.py
@@ -78,9 +78,18 @@ WSGI_APPLICATION = 'MPI_model.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mpi_test',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
