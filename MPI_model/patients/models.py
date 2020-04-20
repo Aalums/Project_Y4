0024a26125d4d5d140efcc967174a162d6a985a3
Aalums <59070194@it.kmitl.ac.th>
2020-04-20 15:35:55 +0700

    Edit model DB

--- MPI_model/patients/models.py
+++ MPI_model/patients/models.py
@@ -6,36 +6,63 @@ class patients(models.Model):
     HN = models.CharField(max_length=9)
     fname = models.CharField(max_length=30)
     lname = models.CharField(max_length=30)
-    gender = models.CharField(max_length=10)
+    gender = models.BinaryField()
 
-class patient_characteristic(models.Model):
-    cha_id = models.AutoField(primary_key=True)
-    cha_date = models.DateTimeField(auto_now_add=True, blank=True)
-    age = models.IntegerField(null=True)
+class patient_info(models.Model):
+    info_id = models.AutoField(primary_key=True)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+    age = models.IntegerField()
     bmi = models.DecimalField(max_digits=20, decimal_places=2)
-    dm = models.CharField(max_length=1)
-    ht = models.CharField(max_length=1)
-    dlp = models.CharField(max_length=1)
-    ckd = models.CharField(max_length=1)
-    pid = models.ForeignKey(patients, on_delete=models.CASCADE)
-
-class patient_mpi(models.Model):
-    mpi_id = models.AutoField(primary_key=True)
-    mpi_date = models.DateTimeField(auto_now_add=True, blank=True)
+    dm = models.BinaryField()
+    ht = models.BinaryField()
+    dlp = models.BinaryField()
+    ckd = models.BinaryField()
     # LAD
     LAD_4dmspect = models.DecimalField(max_digits=20, decimal_places=2)
     LAD_wallthick = models.DecimalField(max_digits=20, decimal_places=2)
     LAD_wallmotion = models.DecimalField(max_digits=20, decimal_places=2)
-    LAD_CAG = models.CharField(max_length=1)
+    LAD_CAG = models.BinaryField()
     # LCX
     LCX_4dmspect = models.DecimalField(max_digits=20, decimal_places=2)
     LCX_wallthick = models.DecimalField(max_digits=20, decimal_places=2)
     LCX_wallmotion = models.DecimalField(max_digits=20, decimal_places=2)
-    LCX_CAG = models.CharField(max_length=1)
+    LCX_CAG = models.BinaryField()
     # RCA
     RCA_4dmspect = models.DecimalField(max_digits=20, decimal_places=2)
     RCA_wallthick = models.DecimalField(max_digits=20, decimal_places=2)
     RCA_wallmotion = models.DecimalField(max_digits=20, decimal_places=2)
-    RCA_CAG = models.CharField(max_length=1)
+    RCA_CAG = models.BinaryField()
     LVEF = models.IntegerField()
-    char_id = models.ForeignKey(patient_characteristic, on_delete=models.CASCADE)
+    pid = models.ForeignKey(patients, on_delete=models.CASCADE)
+
+# class patient_characteristic(models.Model):
+#     cha_id = models.AutoField(primary_key=True)
+#     cha_date = models.DateTimeField(auto_now_add=True, blank=True)
+#     age = models.IntegerField(null=True)
+#     bmi = models.DecimalField(max_digits=20, decimal_places=2)
+#     dm = models.CharField(max_length=1)
+#     ht = models.CharField(max_length=1)
+#     dlp = models.CharField(max_length=1)
+#     ckd = models.CharField(max_length=1)
+#     pid = models.ForeignKey(patients, on_delete=models.CASCADE)
+
+# class patient_mpi(models.Model):
+#     mpi_id = models.AutoField(primary_key=True)
+#     mpi_date = models.DateTimeField(auto_now_add=True, blank=True)
+#     # LAD
+#     LAD_4dmspect = models.DecimalField(max_digits=20, decimal_places=2)
+#     LAD_wallthick = models.DecimalField(max_digits=20, decimal_places=2)
+#     LAD_wallmotion = models.DecimalField(max_digits=20, decimal_places=2)
+#     LAD_CAG = models.CharField(max_length=1)
+#     # LCX
+#     LCX_4dmspect = models.DecimalField(max_digits=20, decimal_places=2)
+#     LCX_wallthick = models.DecimalField(max_digits=20, decimal_places=2)
+#     LCX_wallmotion = models.DecimalField(max_digits=20, decimal_places=2)
+#     LCX_CAG = models.CharField(max_length=1)
+#     # RCA
+#     RCA_4dmspect = models.DecimalField(max_digits=20, decimal_places=2)
+#     RCA_wallthick = models.DecimalField(max_digits=20, decimal_places=2)
+#     RCA_wallmotion = models.DecimalField(max_digits=20, decimal_places=2)
+#     RCA_CAG = models.CharField(max_length=1)
+#     LVEF = models.IntegerField()
+#     char_id = models.ForeignKey(patient_characteristic, on_delete=models.CASCADE)
