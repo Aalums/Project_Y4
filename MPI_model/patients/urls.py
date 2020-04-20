7b247be5df81c0f04ca12edbb9010f54705ebdba
phpond <59070182@it.kmitl.ac.th>
2020-04-20 15:57:33 +0700

    addpatients

--- MPI_model/patients/urls.py
+++ MPI_model/patients/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.patientList, name="patient_list"),
-    url(r'^addpatient$', views.addpatient, name="add_patient"),
+    url(r'^/addpatient$', views.addpatient, name="add_patient"),
     url(r'^patientinfo$', views.patientInfo, name="patient_info"),
 ]
