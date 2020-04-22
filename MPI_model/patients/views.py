1c23c9db69cdecfe1f07af269ec3fe79facefed1
phpond <59070182@it.kmitl.ac.th>
2020-04-22 21:35:18 +0700

    insert patient to db success!

--- MPI_model/patients/views.py
+++ MPI_model/patients/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
 from .models import patients, patient_info
 
 # Create your views here.
 def addpatient(request):
-    print(request)
-    header_str = 'Hello'
-    template = loader.get_template('addpatient.html')
-    context = {
-        'var1': header_str
-    }
-    return HttpResponse(template.render(context, request))
+    if request.method == 'POST':
+        patient = patients()
+        patient.HN = request.POST.get('HN_number')
+        patient.fname = request.POST.get('firstName')
+        patient.lname = request.POST.get('lastName')
+        patient.gender = 0 if request.POST.get('gender') == 'male' else 1
+        patient.save()
+        
+        pid = patients.objects.filter(HN=request.POST.get('HN_number'))[0].pid
+        return HttpResponseRedirect('patientinfo/?pid='+str(pid))
+    else : 
+        print(request)
+        header_str = 'Hello'
+        template = loader.get_template('addpatient.html')
+        context = {
+            'var1': header_str
+        }
+        return HttpResponse(template.render(context, request))
 
 def patientList(request):
     print(request)
@@ -22,24 +34,10 @@ def patientList(request):
 
 def patientInfo(request):
     print(request)
-    if request.method == 'POST':
-        patient = patients()
-        patient.HN = request.POST.get('HN_number')
-        # patient.date = request.POST.get('date')
-        patient.fname = request.POST.get('firstName')
-        patient.lname = request.POST.get('lastName')
-        # patient.age = request.POST.get('age')
-        patient.gender = 0 if request.POST.get('gender') == 'male' else 1
-        patient.save()
-
-        pid_1 = patients.objects.filter(request.POST.get('HN_number')).pid
-        patientInfo(Request(method='GET', pid=pid_1))
-
-    else :
-        patient_id = int(request.GET.get('pid'))
-        print("pid = " + str(patient_id))
-        context = {
-            'patient': patients.objects.filter(pid = patient_id).first(),
-            'patient_info': list(patient_info.objects.filter(pid = patient_id))
-        }
-        return render(request, 'patient_info.html', context)
+    patient_id = int(request.GET.get('pid'))
+    print("pid = " + str(patient_id))
+    context = {
+        'patient': patients.objects.filter(pid = patient_id).first(),
+        'patient_info': list(patient_info.objects.filter(pid = patient_id))
+    }        
+    return render(request, 'patient_info.html', context)
