88ff71b2bf9f563bf6f9929266e606a3200aeee2
phpond <59070182@it.kmitl.ac.th>
2020-04-22 16:28:31 +0700

    add patients

--- MPI_model/patients/views.py
+++ MPI_model/patients/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from patients.models import patients, patient_info
+from .models import patients, patient_info
 
 # Create your views here.
 def addpatient(request):
+    print(request)
     header_str = 'Hello'
     template = loader.get_template('addpatient.html')
     context = {
@@ -13,16 +14,32 @@ def addpatient(request):
     return HttpResponse(template.render(context, request))
 
 def patientList(request):
+    print(request)
     context = {
         'list_patient': list(patients.objects.all())
     }
     return render(request, 'patient_list.html', context)
 
 def patientInfo(request):
-    patient_id = int(request.GET.get('pid'))
-    print("pid = " + str(patient_id))
-    context = {
-        'patient': patients.objects.filter(pid = patient_id).first(),
-        'patient_info': list(patient_info.objects.filter(pid = patient_id))
-    }
-    return render(request, 'patient_info.html', context)
+    print(request)
+    if request.method == 'POST':
+        patient = patients()
+        patient.HN = request.POST.get('HN_number')
+        # patient.date = request.POST.get('date')
+        patient.fname = request.POST.get('firstName')
+        patient.lname = request.POST.get('lastName')
+        # patient.age = request.POST.get('age')
+        patient.gender = 0 if request.POST.get('gender') == 'male' else 1
+        patient.save()
+
+        pid_1 = patients.objects.filter(request.POST.get('HN_number')).pid
+        patientInfo(Request(method='GET', pid=pid_1))
+
+    else :
+        patient_id = int(request.GET.get('pid'))
+        print("pid = " + str(patient_id))
+        context = {
+            'patient': patients.objects.filter(pid = patient_id).first(),
+            'patient_info': list(patient_info.objects.filter(pid = patient_id))
+        }
+        return render(request, 'patient_info.html', context)
