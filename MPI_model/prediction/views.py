a4c736a288b1dcffba6e6cac20421b651ceaf865
Aalums <59070194@it.kmitl.ac.th>
2020-05-09 20:30:22 +0700

    Save and Delete Success!!

--- MPI_model/prediction/views.py
+++ MPI_model/prediction/views.py
@@ -5,16 +5,43 @@ from patients.models import patients, patient_info
 
 # Create your views here.
 def prediction(request):
-    print(request)
-    no = int(request.GET.get('no'))
-    patient_id = patients.objects.filter(patient_info__no = no)[0].pid
-    print("No = " + str(no))
-    print("pid = " + str(patient_id))
-    context = {
-        'patient': patients.objects.get(pid = patient_id),
-        'patient_info': patient_info.objects.get(no = no)
-    }
-    return render(request, 'prediction.html', context)
+    if request.method == 'POST':
+        print(request)
+        no = int(request.GET.get('no'))
+        patient_id = patients.objects.filter(patient_info__no = no)[0].pid
+        method = request.POST.get('_method')
+        print(method)
+        if method == 'Save' :
+            CAG_confirm = request.POST.get('CAG_confirm')
+            if CAG_confirm != patient_info.objects.get(no = no).CAG_confirm :
+                print("Update CAG_confirm of No : " + str(no))
+                print("Old : " + str(patient_info.objects.get(no = no).CAG_confirm) + " New : " + str(CAG_confirm))
+                patient_info.objects.filter(no = no).update(CAG_confirm = CAG_confirm)
+            context = {
+            'patient': patients.objects.get(pid = patient_id),
+            'patient_info': patient_info.objects.get(no = no)
+            }
+            return render(request, 'prediction.html', context)
+        else :
+            print("Delete No : " + str(no))
+            patient_info.objects.filter(no = no).delete()
+            context = {
+                'patient': patients.objects.get(pid = patient_id),
+                'patient_info': list(patient_info.objects.filter(pid = patient_id))
+            }
+            return render(request, 'patient_info.html', context)
+
+    else :
+        print(request)
+        no = int(request.GET.get('no'))
+        patient_id = patients.objects.filter(patient_info__no = no)[0].pid
+        print("No = " + str(no))
+        print("pid = " + str(patient_id))
+        context = {
+            'patient': patients.objects.get(pid = patient_id),
+            'patient_info': patient_info.objects.get(no = no)
+        }
+        return render(request, 'prediction.html', context)
 
 def addPredict(request):
     context = {
