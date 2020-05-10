5c3b4edd34cbe758b5021f7cf72192518da915a6
Aalums <59070194@it.kmitl.ac.th>
2020-05-10 15:27:04 +0700

    AddPredict Success!!

--- MPI_model/prediction/views.py
+++ MPI_model/prediction/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from patients.models import patients, patient_info
 
@@ -44,11 +44,38 @@ def prediction(request):
         return render(request, 'prediction.html', context)
 
 def addPredict(request):
-    print(request)
-    patient_id =  int(request.GET.get('pid'))
-    print("pid = " + str(patient_id))
-    context = {
-        'patient': patients.objects.get(pid = patient_id)
-    }
-    return render(request, 'addprediction.html', context)
+    if request.method == 'POST':
+        print(request)
+        patient_id =  int(request.GET.get('pid'))
+        print("Add Info pid = " + str(patient_id))
+        patientInfo = patient_info()
+        patientInfo.age = request.POST.get('age')
+        patientInfo.BMI = request.POST.get('bmi')
+        patientInfo.DM = request.POST.get('dm')
+        patientInfo.HT = request.POST.get('ht')
+        patientInfo.DLP = request.POST.get('dlp')
+        patientInfo.CKD = request.POST.get('ckd')
+        patientInfo.LAD4dmspect = request.POST.get('lad_4dmspect')
+        patientInfo.LADwallthick = request.POST.get('lad_wallthick')
+        patientInfo.LADwallmotion = request.POST.get('lad_wallmotion')
+        patientInfo.LCX4dmspect = request.POST.get('lcx_4dmspect')
+        patientInfo.LCXwallthick = request.POST.get('lcx_wallthick')
+        patientInfo.LCXwallmotion = request.POST.get('lcx_wallmotion')
+        patientInfo.RCA4dmspect = request.POST.get('rca_4dmspect')
+        patientInfo.RCAwallthick = request.POST.get('rca_wallthick')
+        patientInfo.RCAwallmotion = request.POST.get('rca_wallmotion')
+        patientInfo.LVEF = request.POST.get('lvef')
+        patientInfo.pid = patients.objects.get(pid = patient_id)
+        patientInfo.save()
+        
+        no = int(patient_info.objects.latest('no').no)
+        return HttpResponseRedirect('/prediction?no='+str(no))
+    else :
+        print(request)
+        patient_id =  int(request.GET.get('pid'))
+        print("pid = " + str(patient_id))
+        context = {
+            'patient': patients.objects.get(pid = patient_id)
+        }
+        return render(request, 'addprediction.html', context)
 
