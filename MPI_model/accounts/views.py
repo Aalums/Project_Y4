8157b42c6af0536e6ce9d2575e00d7cf6a30f3b7
Aalums <59070194@it.kmitl.ac.th>
2020-05-21 22:44:08 +0700

    Add Logout code

--- MPI_model/accounts/views.py
+++ MPI_model/accounts/views.py
@@ -35,7 +35,8 @@ def login_request(request):
         else:
             print("Someone tried to login and failed.")
             print("They used username: {} and password: {}".format(username,password))
-            return HttpResponse("Invalid login details given")
+            # return HttpResponse("Invalid login details given")
+            return render(request, 'login.html', {})
     else:
         return render(request, 'login.html', {})
 
